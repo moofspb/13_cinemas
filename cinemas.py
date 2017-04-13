9d454c5dc5abca56382a0fb289e36f80d0c67cff
Mikhail Kuntsevich <moofspb@gmail.com>
2017-04-13 23:24:12 +0300

    Add requirements.txt, write fetch_afisha_page and parse_afisha_pages funcs

--- cinemas.py
+++ cinemas.py
@@ -1,12 +1,25 @@
+import requests
+from bs4 import BeautifulSoup
 
 
-def fetch_afisha_page():
-    pass
+def fetch_afisha_page(url='https://www.afisha.ru/spb/schedule_cinema/'):
+    return requests.get(url).text
 
 
 def parse_afisha_list(raw_html):
-    pass
+    soup = BeautifulSoup(raw_html, 'lxml')
+    films = soup.find_all('div', class_='s-votes-hover-area')
+    films_data = []
+    for film in films:
+        title = film.find('h3', class_='usetags').text.strip()
+        cinemas_amount = len(film.find('table').find_all('td', class_='b-td-item'))
+        films_data.append({'title': title, 'cinemas_amount': cinemas_amount})
+    return films_data
 
+
+    #for film in films:
+     #   res.append((film_title, cinemas_amount))
+    #print(res)
 
 def fetch_movie_info(movie_title):
     pass
@@ -17,4 +30,5 @@ def output_movies_to_console(movies):
 
 
 if __name__ == '__main__':
-    pass
+    html = fetch_afisha_page()
+    parse_afisha_list(html)
