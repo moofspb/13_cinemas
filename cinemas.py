534ec7b932c0c24745d58aab1a53d6cb2508b24a
Mikhail Kuntsevich <moofspb@gmail.com>
2017-04-15 15:32:38 +0300

    Add files with lists of user-agents and proxies, add functions 'get_movie_id', 'get_movie_page' and 'fetch_movie_info'

--- cinemas.py
+++ cinemas.py
@@ -1,5 +1,10 @@
 import requests
 from bs4 import BeautifulSoup
+from random import choice
+
+
+USERAGENTS = 'user-agents.txt'
+PROXIES = 'proxies.txt'
 
 
 def fetch_afisha_page(url='https://www.afisha.ru/spb/schedule_cinema/'):
@@ -8,17 +13,43 @@ def fetch_afisha_page(url='https://www.afisha.ru/spb/schedule_cinema/'):
 
 def parse_afisha_list(raw_html):
     soup = BeautifulSoup(raw_html, 'lxml')
-    films = soup.find_all('div', class_='s-votes-hover-area')
-    films_data = []
-    for film in films:
-        title = film.find('h3', class_='usetags').text.strip()
-        cinemas_amount = len(film.find('table').find_all('td', class_='b-td-item'))
-        films_data.append({'title': title, 'cinemas_amount': cinemas_amount})
-    return films_data
+    movies = soup.find_all('div', class_='s-votes-hover-area')
+    movies_data = []
+    for movie in movies:
+        title = movie.find('h3', class_='usetags').text.strip()
+        cinemas_amount = len(movie.find('table').find_all('td', class_='b-td-item'))
+        movies_data.append({'title': title, 'cinemas_amount': cinemas_amount})
+    return movies_data
 
 
-def fetch_movie_info(movie_title):
-    pass
+def get_movie_id(movie_title, year='2017', url='http://kparser.pp.ua/json/search/'):
+    movies_page = requests.get(url+movie_title)
+    movies_data = movies_page.json()
+    movie_info = filter(lambda movie: movie['title'] == movie_title and
+                        movie['year'] == year, movies_data['result'])
+    return list(movie_info)[0]['id']
+
+
+def get_movie_page(movie_id, url='https://www.kinopoisk.ru/film/'):
+    useragents = open(USERAGENTS).read().split('\n')
+    proxies = open(PROXIES).read().split('\n')
+    useragent = {'User-Agent': choice(useragents)}
+    proxy = {'http': 'http://' + choice(proxies)}
+    return requests.get(url + movie_id, headers=useragent, proxies=proxy).text
+
+
+def fetch_movie_info(movie_page):
+    soup = BeautifulSoup(movie_page, 'lxml')
+    movie_title = soup.find('h1', class_='moviename-big').text.strip()
+    rating = soup.find('span', class_='rating_ball').text.strip()
+    votes = soup.find('span', class_='ratingCount').text.strip()
+    return {'title': movie_title,
+            'rating': rating,
+            'votes': votes}
+
+
+def get_movies_data():
+
 
 
 def output_movies_to_console(movies):
@@ -26,5 +57,9 @@ def output_movies_to_console(movies):
 
 
 if __name__ == '__main__':
-    html = fetch_afisha_page()
-    parse_afisha_list(html)
+    #html = fetch_afisha_page()
+    #movies_list = parse_afisha_list(html)
+    #film_id = get_movie_id('Время первых')
+    #fetch_movie_info(film_id)
+    page = get_movie_page('53523')
+    fetch_movie_info(page)
